023f0a50f43724ec62519625968610bb3a174a30
Ngoc Han Ngo <161391723+ngongochan@users.noreply.github.com>
2025-06-20 13:27:16 +0000

    [automatic commit] 2025-06-20 13:27:16 UTC 20.227.141.208

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -14,6 +14,7 @@ router.get('/', async function(req, res) {
             WalkRequests.location,
             Users.username AS owner_username
         FROM WalkRequests
+        JOIN Dogs ON Dogs.dog_id = Users.dog_id
         JOIN Users ON Dogs.owner_id = Users.user_id
         `);
         res.json(results);
