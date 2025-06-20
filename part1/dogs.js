6c75f99e2f536e5b6c507b4aa9daac7691cc4977
Ngoc Han Ngo <161391723+ngongochan@users.noreply.github.com>
2025-06-20 02:36:01 +0000

    [automatic commit] 2025-06-20 02:36:01 UTC 20.227.140.176

--- part1/dogs.js
+++ part1/dogs.js
@@ -1,14 +1,14 @@
 const { createApp } = Vue;
-    createApp({
-      el: #app,
-      data: {
-          dogImage: null
-      },
-      methods: {
-        getDog() {
-            const result = fetch('https://dog.ceo/api/breeds/image/random');
-            const data = res.json();
-            this.dogImage = data.message;
-        }
-      }
-    }).mount('#app');
+createApp({
+    el: #app,
+    data: {
+        dogImage: null
+    },
+    methods: {
+    getDog() {
+        const result = fetch('https://dog.ceo/api/breeds/image/random');
+        const data = res.json();
+        this.dogImage = data.message;
+    }
+    }
+}).mount('#app');
