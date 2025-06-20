9e5a4269c2563bad0c7548700d2a0a0403bcaba1
Ngoc Han Ngo <161391723+ngongochan@users.noreply.github.com>
2025-06-20 18:50:21 +0000

    [automatic commit] 2025-06-20 18:50:21 UTC 20.227.141.208

--- part2/models/db.js
+++ part2/models/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: '127.0.0.1',
+    host: '127.0.0.1',  // not 
     user: 'root',
     database: 'DogWalkService',
     waitForConnections: true,
