258d6c47b6778902112d0ca66d7d600aed48c1c1
Ngoc Han Ngo <161391723+ngongochan@users.noreply.github.com>
2025-06-20 13:51:01 +0000

    [automatic commit] 2025-06-20 13:51:01 UTC 20.227.141.208

--- part1/db/dogwalks.sql
+++ part1/db/dogwalks.sql
@@ -57,7 +57,7 @@ CREATE TABLE WalkRatings (
 
 
 
-// populate data
+-- populate data
 INSERT INTO Users (username, email, password_hash, role) VALUES
 ('alice123', 'alice@example.com', 'hashed123', 'owner'),
 ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
