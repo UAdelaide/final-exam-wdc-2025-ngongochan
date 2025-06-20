66044f29088e03d4b272522a9fe1f7f922f155bf
Ngoc Han Ngo <161391723+ngongochan@users.noreply.github.com>
2025-06-20 22:40:22 +0000

    [automatic commit] 2025-06-20 22:40:22 UTC 20.227.141.208

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models/db'); // adjust if needed
-const session = require('express-session');
+// const session = require('express-session');
 
 // GET /api/dogs - return ALL dogs
 router.get('/', async (req, res) => {
