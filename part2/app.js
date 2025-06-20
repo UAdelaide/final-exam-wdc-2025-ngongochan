cf51a5d6c1cdb1136f00481a9845ee240f1b64fd
Ngoc Han Ngo <161391723+ngongochan@users.noreply.github.com>
2025-06-20 21:10:06 +0000

    [automatic commit] 2025-06-20 21:10:06 UTC 20.227.141.208

--- part2/app.js
+++ part2/app.js
@@ -88,31 +88,31 @@ app.post('/login', async (req, res) => {
   // debugging SQL NULL parameters
   console.log('request body:', req.body);
   if (typeof username === 'undefined' || typeof password === 'undefined') {
-      return res.status(400).json({ error: 'Username and password are required.' });
+    return res.status(400).json({ error: 'Username and password are required.' });
   }
 
   try {
-      const [rows] = await db.execute(
-          'SELECT * FROM Users WHERE username = ? AND password_hash = ?',
-          [username, password]
-      );
-      if (rows.length === 0) {
-          return res.status(401).send('Invalid username or password');
-      }
-      const user = rows[0];
-      req.session.user_id = user.user_id;
-      req.session.role = user.role;
+    const [rows] = await db.execute(
+      'SELECT * FROM Users WHERE username = ? AND password_hash = ?',
+      [username, password]
+    );
+    if (rows.length === 0) {
+      return res.status(401).send('Invalid username or password');
+    }
+    const user = rows[0];
+    req.session.user_id = user.user_id;
+    req.session.role = user.role;
 
-      // redirect based on the user's role
-      if (user.role === 'owner') {
-          // app.use(express.static(path.join(__dirname, '/public')));
-          // pay attention to the path, DO NOT include /public!!
-          res.redirect('/owner-dashboard.html');
-      } else if (user.role === 'walker') {
-          res.redirect('/walker-dashboard.html');
-      } else {
-          res.redirect('/');
-      }
+    // redirect based on the user's role
+    if (user.role === 'owner') {
+        // app.use(express.static(path.join(__dirname, '/public')));
+        // pay attention to the path, DO NOT include /public!!
+        res.redirect('/owner-dashboard.html');
+    } else if (user.role === 'walker') {
+        res.redirect('/walker-dashboard.html');
+    } else {
+        res.redirect('/');
+    }
   } catch (err) {
       console.error(err);
       res.status(500).json({ error: 'Something went wrong' });
