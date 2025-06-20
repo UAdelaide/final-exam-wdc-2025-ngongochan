8156774160d9b2fb05c820d2cf688c6717940f27
Ngoc Han Ngo <161391723+ngongochan@users.noreply.github.com>
2025-06-20 11:43:29 +0000

    [automatic commit] 2025-06-20 11:43:29 UTC 20.227.141.209

--- part1/app.js
+++ part1/app.js
@@ -78,6 +78,48 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth');
+var eventRouter = require('./routes/event');
+var mailRouter = require('./routes/mail');
+var adminRouter = require('./routes/admin');
+
+
+
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// hard code WDC_113 need to put in .env file
+app.use(session({
+  secret: 'WDC_113',
+  resave: false ,
+  saveUninitialized: true,
+  cookie: {
+    secure: false,
+    maxAge: 1000 * 60 * 60 * 2
+  }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/auth', authRouter);
+app.use('/event', eventRouter);
+app.use('/mail', mailRouter);
+app.use('/admin', adminRouter);
+
 
 
 
