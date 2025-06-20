ff21c582f6c59ebdbe8d0204700f8fce44c73f2f
Ngoc Han Ngo <161391723+ngongochan@users.noreply.github.com>
2025-06-20 02:36:41 +0000

    [automatic commit] 2025-06-20 02:36:41 UTC 20.227.140.176

--- part1/dogs.js
+++ part1/dogs.js
@@ -1,21 +1,20 @@
-  const { createApp } = Vue;
+const { createApp } = Vue;
 
-  createApp({
-    data() {
-      return {
-        dogImage: null
-      };
-    },
-    methods: {
-      async getDog() {
-        try {
-          const res = await fetch('https://dog.ceo/api/breeds/image/random');
-          const data = await res.json();
-          this.dogImage = data.message;
-        } catch (error) {
-          console.error("Failed to fetch dog image:", error);
-        }
-      }
+createApp({
+data() {
+    return {
+    dogImage: null
+    };
+},
+methods: {
+    async getDog() {
+    try {
+        const res = await fetch('https://dog.ceo/api/breeds/image/random');
+        const data = await res.json();
+        this.dogImage = data.message;
+    } catch (error) {
+        console.error("Failed to fetch dog image:", error);
     }
-  }).mount('#app');
-</script>
+    }
+}
+}).mount('#app');
