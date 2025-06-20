f8ed053d4a2e7c786febdeb5f99a90d3aaf004bc
Ngoc Han Ngo <161391723+ngongochan@users.noreply.github.com>
2025-06-20 11:47:50 +0000

    [automatic commit] 2025-06-20 11:47:50 UTC 20.227.141.209

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -1,6 +1,11 @@
 const express = require('express');
 var router = express.Router();
 
+
+
+// "dog_name": "Max",
+//     "size": "medium",
+//     "owner_username": "alice123"
 router.get('/api/dogs', async function(req, res) {
     try {
         const email = req.query.email;
