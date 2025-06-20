2f51f7dcc454dbf779bf85bbf60644f6dccba732
Ngoc Han Ngo <161391723+ngongochan@users.noreply.github.com>
2025-06-20 13:52:01 +0000

    [automatic commit] 2025-06-20 13:52:01 UTC 20.227.141.208

---
+++ part1/routes/walkers.js
@@ -0,0 +1,28 @@
+const express = require('express');
+var router = express.Router();
+var db = require('../db/db');
+
+// /api/dogs route
+// (request_id, dog_name, requested_time, duration_minutes, location, owner_username)
+router.get('/', async function(req, res) {
+    try {
+        const [results] = await db.query(`
+        SELECT
+            WalkRequests.request_id,
+            Dogs.name AS dog_name,
+            WalkRequests.requested_time,
+            WalkRequests.duration_minutes,
+            WalkRequests.location,
+            Users.username AS owner_username
+        FROM WalkRequests
+        JOIN Dogs ON Dogs.dog_id = WalkRequests.dog_id
+        JOIN Users ON Dogs.owner_id = Users.user_id
+        `);
+        res.json(results);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to fetch walkers' });
+    }
+});
+
+module.exports = router;
