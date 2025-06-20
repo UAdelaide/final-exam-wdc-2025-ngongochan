c4e3f55348203f4d0a46827777eccb2ce3a47c64
Ngoc Han Ngo <161391723+ngongochan@users.noreply.github.com>
2025-06-20 11:46:50 +0000

    [automatic commit] 2025-06-20 11:46:50 UTC 20.227.141.209

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -1,2 +1,25 @@
 const express = require('express');
 var router = express.Router();
+
+router.get('/api/dogs', async function(req, res) {
+    try {
+        const email = req.query.email;
+        const title = req.query.title;
+        console.log(email);
+        console.log(title);
+        let [users] = await db.query("SELECT * FROM users;");
+        let [events] = await db.query("SELECT * FROM events;");
+        if (email) {
+            [users] = await db.query("SELECT * FROM users WHERE email LIKE ?;", [`%${email}%`]);
+        }
+        if (title) {
+            [events] = await db.query("SELECT * FROM events WHERE title LIKE ?;", [`%${title}%`]);
+        }
+        res.render('admin', {
+            users: users,
+            events: events
+        });
+    } catch(err) {
+        res.render('error');
+    }
+});
