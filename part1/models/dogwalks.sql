f52d1ed28db3bd653037e22ccf1182571da3f6f6
Ngoc Han Ngo <161391723+ngongochan@users.noreply.github.com>
2025-06-20 11:16:04 +0000

    [automatic commit] 2025-06-20 11:16:04 UTC 20.227.141.209

--- part1/models/dogwalks.sql
+++ part1/models/dogwalks.sql
@@ -62,18 +62,9 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
 ('alice123', 'alice@example.com', 'hashed123', 'owner'),
 ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
 ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-('walkerkrixi', 'krixi@example.com', 'hashed012', 'walker')
+('walkerkrixi', 'krixi@example.com', 'hashed012', 'walker'),
+('taara', 'taara@example.com', 'hashed456', 'walker')
 ;
-
-
-
-INSERT INTO Users (username, email, password_hash, role)
-VALUES ('walkerkrixi', 'krixi@example.com', 'hashed012', 'walker');
-
-INSERT INTO Users (username, email, password_hash, role)
-VALUES ('taara', 'taara@example.com', 'hashed456', 'walker');
-
-
 
 INSERT INTO Dogs (name, size, owner_id)
 VALUES (
