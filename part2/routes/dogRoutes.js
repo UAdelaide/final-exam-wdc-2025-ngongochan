74c95a8ed93c0b6b518624167b912c67163d1d46
Ngoc Han Ngo <161391723+ngongochan@users.noreply.github.com>
2025-06-21 06:20:06 +0000

    [automatic commit] 2025-06-21 06:20:06 UTC 20.227.141.210

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -8,28 +8,11 @@ router.get('/', async (req, res) => {
     const [dogs] = await db.execute('SELECT * FROM Dogs');
     res.json(dogs);
   } catch (err) {
-    console.error('SQL Error:', err);
+    console.error(err);
     res.status(500).json({ error: 'Failed to fetch dogs' });
   }
 });
 
-// /api/dogs route  (dog_name, size, owner_username)
-router.get('/', async function(req, res) {
-    try {
-        const [results] = await db.query(`
-        SELECT
-            Dogs.name AS dog_name,
-            Dogs.size,
-            Users.username AS owner_username
-        FROM Dogs
-        JOIN Users ON Dogs.owner_id = Users.user_id
-        `);
-        res.json(results);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Failed to fetch dogs' });
-    }
-});
 
 
 
