0f0cc508d1f868ab53318b71260413c7328accf7
Ngoc Han Ngo <161391723+ngongochan@users.noreply.github.com>
2025-06-21 06:27:27 +0000

    [automatic commit] 2025-06-21 06:27:27 UTC 20.227.141.210

--- part2/app.js
+++ part2/app.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
-var db = require('./models'); // ?
+var db = require('./models/db'); // ?
 
 (async () => {
   try {
